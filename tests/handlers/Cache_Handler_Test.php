d28e52828eb1327f315cb9a60a78bf60d4701fff
DamoFD <voshall@yahoo.com>
2024-07-30 23:10:48 -0700

    comments on cache handler tests

--- tests/handlers/Cache_Handler_Test.php
+++ tests/handlers/Cache_Handler_Test.php
@@ -5,19 +5,48 @@ namespace WP_Swapper\Tests\Handlers;
 use WP_Swapper\Tests\TestCase;
 use WP_Swapper\Traits\Cache_Handler;
 
+/**
+* Cache Handler Tests
+*
+* @since 0.0.1
+*/
 class Cache_Handler_Test extends TestCase {
 
     use Cache_Handler;
 
+    /**
+    * Mock html content
+    *
+    * @since 0.0.1
+    *
+    * @var string
+    */
     private $content = '<div>Test Content</div>';
 
+    /**
+    * Mock session key
+    *
+    * @since 0.0.1
+    *
+    * @var string
+    */
     private $key = 'test_component';
 
+    /**
+    * Initialize empty $_SESSION array
+    *
+    * @since 0.0.1
+    */
     protected function setUp(): void {
         parent::setUp();
         $_SESSION = [];
     }
 
+    /**
+    * Clean up $_SESSION array after test completion
+    *
+    * @since 0.0.1
+    */
     protected function tearDown(): void {
         $_SESSION = [];
         parent::tearDown();
@@ -58,6 +87,16 @@ class Cache_Handler_Test extends TestCase {
         $this->assertFalse($this->hasComponentChanged($this->key, $this->content), 'Component should not be marked as changed.');
     }
 
+    /**
+    * Mock normalizeContent function to satisfy
+    * abstract method in Cache_Handler
+    *
+    * @since 0.0.1
+    *
+    * @param string $content
+    *
+    * @return string $content
+    */
     protected function normalizeContent($content) {
         return $content;
     }
